a909da35c1ab563fa8fb07716c5bf3cca8324095
myunusa <myunusahja1@gmail.com>
2022-11-30 18:44:55 +0100

    heruku

--- Catering/settings.py
+++ Catering/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path, os
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -96,6 +99,10 @@ DATABASES = {
         'PORT': '7007',
     }
 }
+
+DATABASES = {'default': dj_database_url.config()}
+
+django_heroku.settings(locals())
 
 
 # Password validation
