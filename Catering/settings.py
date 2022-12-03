500ecfba2569dcf297a59f2f8a95ebd210fae847
myunusa <myunusahja1@gmail.com>
2022-12-03 19:24:06 +0100

    gghh

--- Catering/settings.py
+++ Catering/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4)!wlaho=h7(_*t30notc&i8v8k0po8rs_wx5oj20=yyx49s(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # if os.environ['DEBUG']:
 #     print True
