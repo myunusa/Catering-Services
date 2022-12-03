47f7e3c2e4fc96d2681fa4fb146ded247277de84
myunusa <myunusahja1@gmail.com>
2022-12-03 19:19:34 +0100

    gghh

--- Catering/settings.py
+++ Catering/settings.py
@@ -33,6 +33,7 @@ DEBUG = False
 #     print False
 
 ALLOWED_HOSTS = ["https://catering-services.herokuapp.com","https://catering-services-production.up.railway.app",'*']
+CSRF_TRUSTED_ORIGINS =['https://catering-services-production.up.railway.app']
 
 
 # Application definition
@@ -88,6 +89,7 @@ WSGI_APPLICATION = 'Catering.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
